e30e9589d19011597d8de9ea7ac78007310d80dd
surajrupnar <suraj@MacBookAir.lan>
2025-03-02 15:25:24 +0530

    Method - 2 @component - default bean name

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -10,7 +10,12 @@ public class AnnotationDemoApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		//get the bean from the spring container
-		Coach theCoach = context.getBean("thatsillycoach", Coach.class);
+		//Method -1 - @Component("thatsillycoach")
+		//Coach theCoach = context.getBean("thatsillycoach", Coach.class);
+
+		//Method - 2 @component - default bean name
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
 		
 		
 		//call the method on the bean
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,8 +1,11 @@
 package com.luv2code.springdemo;
 
 import org.springframework.stereotype.Component;
+//Method -1 - @Component("thatsillycoach")
+//@Component("thatsillycoach")
 
-@Component("thatsillycoach")
+//Method - 2 @component - default bean name
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
